20a5842f73d576f17baa23b439aeb5f72d7bdf9a
roy-kisholoy <kisholoy@yeasitech.com>
2023-05-22 14:37:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import fakeData from "./MOCK_DATA.json";
+import * as React from "react";
+// import { useTable } from "react-table";
+import Table from "./Table";
 
 function App() {
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const data = React.useMemo(() => fakeData, []);
+
+  const filterData = (data, searchTerm) => {
+    return data.filter((row) => {
+      return (
+        row.first_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        row.last_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        row.email.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        row.university.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    });
+  };
+
+  const sortBy = (column) => {
+    return (data, sortOrder) => {
+      if (sortOrder === "asc") {
+        return data.sort((a, b) => a[column].localeCompare(b[column]));
+      } else {
+        return data.sort((a, b) => b[column].localeCompare(a[column]));
+      }
+    };
+  };
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "ID",
+        accessor: "id",
+        sortBy: sortBy("id"),
+      },
+      {
+        Header: "First Name",
+        accessor: "first_name",
+        sortBy: sortBy("first_name"),
+      },
+      {
+        Header: "Last Name",
+        accessor: "last_name",
+        sortBy: sortBy("last_name"),
+      },
+      {
+        Header: "Email",
+        accessor: "email",
+        sortBy: sortBy("email"),
+      },
+      {
+        Header: "Gender",
+        accessor: "gender",
+        sortBy: sortBy("gender"),
+      },
+      {
+        Header: "University",
+        accessor: "university",
+        sortBy: sortBy("university"),
+      },
+    ],
+    []
+  );
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  // const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+  //   useTable({ columns, data });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        className="search-field"
+        value={searchTerm}
+        onChange={handleChange}
+        placeholder="Search..."
+      />
+      <Table columns={columns} data={filterData(data, searchTerm)} />
     </div>
   );
 }
---
+++ src/Table.js
@@ -0,0 +1,102 @@
+import React from "react";
+import { useTable, useSortBy, usePagination } from "react-table";
+
+export default function Table({ columns, data, pageChange }) {
+  // Use the useTable Hook to send the columns and data to build the table
+  const {
+    getTableProps, // table props from react-table
+    getTableBodyProps, // table body props from react-table
+    headerGroups, // headerGroups, if your table has groupings
+    page, // rows for the table based on the data passed
+    // The rest of these things are super handy, too ;)
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+    prepareRow, // Prepare the row (this function needs to be called for each row before getting the row props)
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageIndex: 0 },
+    },
+    useSortBy,
+    usePagination
+  );
+
+  return (
+    <>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render("Header")}
+                  <span>
+                    {column.isSorted
+                      ? column.isSortedDesc
+                        ? " 🔽"
+                        : " 🔼"
+                      : ""}
+                  </span>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody className="page" {...getTableBodyProps()}>
+          {page.map((row, i) => {
+            prepareRow(row); // This line is necessary to prepare the rows and get the row props from react-table dynamically
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div className="pagination">
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {"<<"}
+        </button>{" "}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {"<"}
+        </button>{" "}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {">"}
+        </button>{" "}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {">>"}
+        </button>{" "}
+        <span>
+          Page{" "}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{" "}
+        </span>
+        <select
+          value={pageSize}
+          onChange={(e) => {
+            setPageSize(Number(e.target.value));
+          }}
+        >
+          {[5, 10].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+    </>
+  );
+}
